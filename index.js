6bd901321fcf882a2e630fbaf345eed4b574255b
kahafMd <103684304+kahafMd@users.noreply.github.com>
2023-05-13 11:41:56 +0530

    Update index.js (added sound to drums)

--- index.js
+++ index.js
@@ -37,43 +37,43 @@ function makeSound(key) {
     switch (key) {
         case "w":
 
-            let audio1 = new Audio("/sounds/crash.mp3")
+            let audio1 = new Audio("/Drum-kit/sounds/crash.mp3")
             audio1.play();
 
             break;
         case "a":
 
-            let audio2 = new Audio("/sounds/kick-bass.mp3")
+            let audio2 = new Audio("/Drum-kit/sounds/kick-bass.mp3")
             audio2.play();
 
             break;
         case "s":
 
-            let audio3 = new Audio("/sounds/snare.mp3")
+            let audio3 = new Audio("/Drum-kit/sounds/snare.mp3")
             audio3.play();
 
             break;
         case "d":
 
-            let audio4 = new Audio("/sounds/tom-1.mp3")
+            let audio4 = new Audio("/Drum-kit/sounds/tom-1.mp3")
             audio4.play();
 
             break;
         case "j":
 
-            let audio5 = new Audio("/sounds/tom-2.mp3")
+            let audio5 = new Audio("/Drum-kit/sounds/tom-2.mp3")
             audio5.play();
 
             break;
         case "k":
 
-            let audio6 = new Audio("/sounds/tom-3.mp3")
+            let audio6 = new Audio("/Drum-kit/sounds/tom-3.mp3")
             audio6.play();
 
             break;
         case "l":
 
-            let audio7 = new Audio("/sounds/tom-4.mp3")
+            let audio7 = new Audio("/Drum-kit/sounds/tom-4.mp3")
             audio7.play();
 
             break;
